84a1d54cca239620f5e92431f5e24dbd54a2b0bf
unknown <thushiravirajan01@gmail.com>
2022-06-07 13:00:20 +0530

    GRPC Client

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Net.Http;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            string customerType = "DIAMOND";
+            var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpHandler = httpHandler });
+            var client = new CalculateDiscountAmount.CalculateDiscountAmountClient(channel);
+            var request = new CalculateRequest { Customertype = customerType };
+            var reply = client.AmountCalculate(request);
+
+            Console.WriteLine($"Discount for customer type {customerType}  is {(reply.Customerdiscount)}");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
